765ebb79c9bf40c2d2d1fe5fb6654cca326d6135
Philippe Lafrance <phil.laf@gmail.com>
2015-12-04 07:28:53 -0500

    Set the role of the newly registered user in session variable

--- src/Listener/RoleListener.php
+++ src/Listener/RoleListener.php
@@ -5,6 +5,7 @@ namespace Permissions\Listener;
 use Cake\Event\Event;
 use Cake\Event\EventListenerInterface;
 use Cake\ORM\TableRegistry;
+use Permissions\Model\Entity\Role;
 
 class RoleListener implements EventListenerInterface
 {
@@ -15,8 +16,10 @@ class RoleListener implements EventListenerInterface
     {
         return [
             'Auth.afterIdentify' => 'afterIdentify',
+            'Users.afterSignup' => 'afterSignup',
         ];
     }
+
     /**
      * Automaticaly adds an aro for each subscribed user
      */
@@ -26,5 +29,15 @@ class RoleListener implements EventListenerInterface
         $role = $rolesTable->find()->where(['user_id' => $user['id']])->first();
         $user['role'] = $role['role'];
         return $user;
+    }
+
+    /**
+     * Automaticaly adds an aro for each subscribed user
+     */
+    public function afterSignup(Event $event, array $user)
+    {
+        $user = $event->subject->Auth->user();
+        $user['role'] = Role::USER;
+        $event->subject->Auth->setUser($user);
     }
 }
